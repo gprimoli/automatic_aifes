0e2065c02d23e12414d4d6f29a67bbf70d30d368
Gennaro <grimoli@unisa.it>
2025-03-17 10:51:44 +0100

    dinamic call

--- lib/GenericFuncs.h
+++ lib/GenericFuncs.h
@@ -3,18 +3,55 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #define ARRAY_LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
+#define BUFFER_SIZE 4096
 
-void handle_error(char *format, ...) {
-    va_list args;
-    va_start(args, format);
+int saveArray(char *fileName, float *fileContent, uint32_t size) {
+    FILE *fp = fopen(fileName == NULL ? "weights" : fileName, "w");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening %s\n", fileName);
+        return EXIT_FAILURE;
+    }
 
-    vprintf(format, args);
-    printf("\n");
+    for (int i = 0; i < size; i++) {
+        fprintf(fp, "%f\n", fileContent[i]);
+    }
+}
 
-    va_end(args);
-    exit(500);
+int readCSV(char *fileName, float *arr, uint32_t size) {
+    FILE *fp = fopen(fileName, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening %s\n", fileName);
+        return EXIT_FAILURE;
+    }
+
+    char line[BUFFER_SIZE];
+    int i = 0;
+    while (fgets(line, BUFFER_SIZE, fp) != NULL) {
+        char *token = strtok(line, ",");
+        do {
+            arr[i++] = atof(token);
+            if (i > size) {
+                fprintf(stderr, "Error reading %s\n", fileName);
+                fclose(fp);
+                return EXIT_FAILURE;
+            }
+        }while ((token = strtok(NULL, ",")) != NULL);
+    }
+
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+
+void safeFree(void** ptr) {
+    if (ptr && *ptr) {
+        free(*ptr);
+        *ptr = NULL;
+    }
 }
 
 #endif //GENERICFUNCS_H
--- mainDinamic.c
+++ mainDinamic.c
@@ -7,29 +7,34 @@
 
 void printLoss(float loss);
 
-int parse_layers(char *arg, uint32_t **LAYER_COMPOSITION, AIFES_E_activations **ACTIVATION_FUNCTION, int *INPUT,
-                 int *OUTPUT, int *TOT_LAYER);
+int parse_layers(char *arg, uint32_t **LAYER_COMPOSITION, uint32_t *TOT_LAYER);
 
-int parse_activation_func(char *arg, AIFES_E_activations *ACTIVATION_FUNCTION, int TOT_LAYER);
+int parse_activation_func(char *arg, AIFES_E_activations **ACTIVATION_FUNCTION, uint32_t TOT_LAYER);
+
+int runTraining(uint32_t *LAYER_COMPOSITION, AIFES_E_activations *ACTIVATION_FUNCTION, uint32_t TOT_LAYER,
+                uint32_t BATCH_SIZE, uint32_t EPOCH, uint32_t LOG_LOSS, float *weights, float *input, float *target,
+                float *output, char *weights_filename);
+
+int initArr(float **arr, uint32_t size, char *initFile);
 
 
 int main(int argc, char *argv[]) {
-    int INPUT;
-    int OUTPUT;
-
-    int BATCH_SIZE;
-    int EPOCH;
-    int LOG_LOSS;
-
-    uint32_t *LAYER_COMPOSITION;
-    AIFES_E_activations *ACTIVATION_FUNCTION;
-    int TOT_LAYER;
-
-
     srand(time(NULL));
-    int opt;
 
-    while ((opt = getopt(argc, argv, "b:e:l:a:x:")) != -1) {
+    uint32_t BATCH_SIZE = 0;
+    uint32_t EPOCH = 0;
+    uint32_t LOG_LOSS = 0;
+    uint32_t TOT_LAYER = 0;
+
+    uint32_t *LAYER_COMPOSITION = NULL;
+    AIFES_E_activations *ACTIVATION_FUNCTION = NULL;
+    float *weights = NULL, *input_data = NULL, *targhet_data = NULL, *output_data = NULL;
+
+    char *weights_filename = NULL, *input_filename = NULL, *targhet_filename = NULL;
+    char *layerDefString = NULL, *activationFunctionString = NULL;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "b:e:l:a:x:w:i:t:")) != -1) {
         switch (opt) {
             case 'b':
                 BATCH_SIZE = atoi(optarg);
@@ -38,232 +43,224 @@ int main(int argc, char *argv[]) {
                 EPOCH = atoi(optarg);
                 break;
             case 'l':
-                if (!parse_layers(optarg, &LAYER_COMPOSITION, &ACTIVATION_FUNCTION, &INPUT, &OUTPUT, &TOT_LAYER)) {
-                    fprintf(stderr, "Error on parse_layers function\n");
-                    return EXIT_FAILURE;
-                }
-
+                layerDefString = optarg;
                 break;
             case 'a':
-                if (!parse_activation_func(optarg, ACTIVATION_FUNCTION, TOT_LAYER)) {
-                    free(LAYER_COMPOSITION);
-                    free(ACTIVATION_FUNCTION);
-                }
+                activationFunctionString = optarg;
                 break;
             case 'x':
                 LOG_LOSS = atoi(optarg);
                 break;
+            case 'w':
+                weights_filename = optarg;
+                break;
+            case 'i':
+                input_filename = optarg;
+                break;
+            case 't':
+                targhet_filename = optarg;
+                break;
             default:
                 fprintf(
                     stderr,
-                    "Usage: %s -b <batch_size> -a <activation_function_composition> -e <epoch> -l <layer_composition>\n",
+                    "Usage: %s -b <batch_size> -e <epoch> -l <layer_composition> -a <activation_function_composition> "
+                    "-x <log_loss> -i <input_file> -t <targhet_file> [-w <weights_file>]\n",
                     argv[0]);
-                return 1;
+                return EXIT_FAILURE;
         }
     }
 
+    if ((parse_layers(layerDefString, &LAYER_COMPOSITION, &TOT_LAYER) != EXIT_SUCCESS) ||
+        (parse_activation_func(activationFunctionString, &ACTIVATION_FUNCTION, TOT_LAYER) != EXIT_SUCCESS) ||
+        (initArr(&weights, AIFES_E_flat_weights_number_fnn_f32(LAYER_COMPOSITION, TOT_LAYER), weights_filename) !=
+         EXIT_SUCCESS) ||
+        (initArr(&input_data, BATCH_SIZE * LAYER_COMPOSITION[0], input_filename) != EXIT_SUCCESS) ||
+        (initArr(&targhet_data, BATCH_SIZE * LAYER_COMPOSITION[TOT_LAYER - 1], targhet_filename) != EXIT_SUCCESS) ||
+        (initArr(&output_data, BATCH_SIZE * LAYER_COMPOSITION[TOT_LAYER - 1], NULL) != EXIT_SUCCESS)
+    ) {
+        safeFree((void **) &LAYER_COMPOSITION);
+        safeFree((void **) &ACTIVATION_FUNCTION);
+        safeFree((void **) &weights);
+        safeFree((void **) &input_data);
+        safeFree((void **) &targhet_data);
+        safeFree((void **) &output_data);
+        return EXIT_FAILURE;
+    }
 
-    printf("Configurazione della Rete Neurale:\n");
-    printf("- Input Neurons: %d\n", INPUT);
-    printf("- Output Neurons: %d\n", OUTPUT);
+    printf("Neural Network Configuration:\n");
+    printf("- Input Neurons: %d\n", LAYER_COMPOSITION[0]);
+    printf("- Output Neurons: %d\n", LAYER_COMPOSITION[TOT_LAYER - 1]);
     printf("- Total Layers: %d\n", TOT_LAYER);
     printf("- Batch Size: %d\n", BATCH_SIZE);
     printf("- Epochs: %d\n", EPOCH);
-    printf("- Log Loss every %d epochs\n", LOG_LOSS);
-    printf("\n");
+    printf("- Log Loss every %d epochs\n\n", LOG_LOSS);
 
-    uint32_t weights_number = AIFES_E_flat_weights_number_fnn_f32(LAYER_COMPOSITION, TOT_LAYER);
-    float *weights = malloc(weights_number * sizeof(double));
-    if (weights == NULL) {
-        free(LAYER_COMPOSITION);
-        free(ACTIVATION_FUNCTION);
-        printf("Memory allocation failed for weights\n");
-        return EXIT_FAILURE;
-    }
+    runTraining(LAYER_COMPOSITION, ACTIVATION_FUNCTION, TOT_LAYER, BATCH_SIZE, EPOCH, LOG_LOSS, weights, input_data, targhet_data, output_data, weights_filename);
 
-
-    /* ---- Wrapper ---- */
-    AIFES_E_model_parameter_fnn_f32 nnStructure;
-    nnStructure.layer_count = TOT_LAYER;
-    nnStructure.fnn_structure = LAYER_COMPOSITION;
-    nnStructure.fnn_activations = ACTIVATION_FUNCTION;
-    nnStructure.flat_weights = weights;
-
-    AIFES_E_init_weights_parameter_fnn_f32 INIT_WEIGHTS;
-    INIT_WEIGHTS.init_weights_method = AIfES_E_init_glorot_uniform; // OR AIfES_E_init_no_init OR AIfES_E_init_uniform
-
-    AIFES_E_training_parameter_fnn_f32 trainStructure;
-    trainStructure.optimizer = AIfES_E_adam; // OR AIfES_E_sgd
-    trainStructure.loss = AIfES_E_mse; // OR AIfES_E_crossentropy
-    trainStructure.learn_rate = 0.05f;
-    trainStructure.batch_size = BATCH_SIZE;
-    trainStructure.epochs = EPOCH;
-    trainStructure.epochs_loss_print_interval = LOG_LOSS;
-    trainStructure.loss_print_function = printLoss;
-    trainStructure.early_stopping = AIfES_E_early_stopping_off; // OR AIfES_E_early_stopping_on
-    trainStructure.early_stopping_target_loss = 0.004f;
-    /* ---- Wrapper ---- */
-
-
-    float *input_data = malloc(BATCH_SIZE * INPUT * sizeof(float));
-    if (input_data == NULL) {
-        free(LAYER_COMPOSITION);
-        free(ACTIVATION_FUNCTION);
-        free(weights);
-        printf("Memory allocation failed for input_data\n");
-        return EXIT_FAILURE;
-    }
-    uint16_t input_shape[] = {BATCH_SIZE, INPUT};
-    aitensor_t input_tensor = AITENSOR_2D_F32(input_shape, input_data);
-
-
-    float *target_data = malloc(BATCH_SIZE * OUTPUT * sizeof(float));
-    if (target_data == NULL) {
-        free(input_data);
-        free(LAYER_COMPOSITION);
-        free(ACTIVATION_FUNCTION);
-        free(weights);
-        printf("Memory allocation failed for target_data\n");
-        return EXIT_FAILURE;
-    }
-    uint16_t target_shape[] = {BATCH_SIZE, OUTPUT};
-    aitensor_t target_tensor = AITENSOR_2D_F32(target_shape, target_data);
-
-
-    float *output_data = malloc(BATCH_SIZE * OUTPUT * sizeof(float));
-    if (output_data == NULL) {
-        free(target_data);
-        free(input_data);
-        free(LAYER_COMPOSITION);
-        free(ACTIVATION_FUNCTION);
-        free(weights);
-        printf("Memory allocation failed for output_data\n");
-        return EXIT_FAILURE;
-    }
-    uint16_t output_shape[] = {BATCH_SIZE, OUTPUT};
-    aitensor_t output_tensor = AITENSOR_2D_F32(output_shape, output_data);
-
-    int8_t error = AIFES_E_training_fnn_f32(&input_tensor, &target_tensor, &nnStructure, &trainStructure, &INIT_WEIGHTS,
-                                            &output_tensor);
-    printf("%d ", error);
-
-    free(input_data);
-    free(output_data);
-    free(target_data);
-    free(LAYER_COMPOSITION);
-    free(ACTIVATION_FUNCTION);
-    free(weights);
-
+    safeFree((void **) &LAYER_COMPOSITION);
+    safeFree((void **) &ACTIVATION_FUNCTION);
+    safeFree((void **) &weights);
+    safeFree((void **) &input_data);
+    safeFree((void **) &targhet_data);
+    safeFree((void **) &output_data);
     return EXIT_SUCCESS;
 }
 
 
-// #define MAX_LINE_LENGTH 256
-//
-// void read_csv(const char *filename);
-// void read_csv(const char *filename) {
-//     FILE *file = fopen(filename, "r");
-//     if (file != NULL) {
-//         char line[MAX_LINE_LENGTH];
-//         int row = 0;
-//
-//         while (fgets(line, sizeof(line), file)) {
-//             row++;
-//             char *token;
-//             int column = 0;
-//
-//             token = strtok(line, ",");
-//             while (token != NULL) {
-//                 column++;
-//                 printf("Riga %d, Colonna %d: %s\n", row, column, token);
-//                 token = strtok(NULL, ",");
-//             }
-//         }
-//         fclose(file);
-//     } else {
-//         exit(EXIT_FAILURE);
-//     }
-// }
-
-
 void printLoss(float loss) {
-    printf("loss: %f\n", loss);
+    printf("Loss: %f\n", loss);
 }
 
-int parse_layers(char *arg, uint32_t **LAYER_COMPOSITION, AIFES_E_activations **ACTIVATION_FUNCTION, int *INPUT,
-                 int *OUTPUT, int *TOT_LAYER) {
+int parse_layers(char *arg, uint32_t **LAYER_COMPOSITION, uint32_t *TOT_LAYER) {
     *TOT_LAYER = 0;
     char *backup = strdup(arg);
     if (backup == NULL) {
+        safeFree((void **) &backup);
+        safeFree((void **) &LAYER_COMPOSITION);
         printf("Memory allocation failed for backup\n");
-        return 0;
+        return EXIT_FAILURE;
     }
-
 
     char *token = strtok(backup, ","); // modifies the original string by replacing delimiters with \0 !!!
-    while (token != NULL) {
+    do {
         (*TOT_LAYER)++;
-        token = strtok(NULL, ",");
-    }
+    } while ((token = strtok(NULL, ",")) != NULL);
 
     *LAYER_COMPOSITION = (uint32_t *) malloc((*TOT_LAYER) * sizeof(uint32_t));
     if (*LAYER_COMPOSITION == NULL) {
-        free(backup);
+        safeFree((void **) &backup);
+        safeFree((void **) &LAYER_COMPOSITION);
         printf("Memory allocation failed for LAYER_COMPOSITION\n");
-        return 0;
-    }
-
-    *ACTIVATION_FUNCTION = (AIFES_E_activations *) malloc((*TOT_LAYER - 1) * sizeof(AIFES_E_activations));
-    if (*ACTIVATION_FUNCTION == NULL) {
-        free(backup);
-        free(*LAYER_COMPOSITION);
-        printf("Memory allocation failed for ACTIVATION_FUNCTION\n");
-        return 0;
+        return EXIT_FAILURE;
     }
 
     strcpy(backup, arg);
     token = strtok(backup, ",");
     for (int i = 0; i < *TOT_LAYER; i++) {
-        if (i == 0) {
-            *INPUT = atoi(token);
-        } else if (i == *TOT_LAYER - 1) {
-            *OUTPUT = atoi(token);
-        }
-
         (*LAYER_COMPOSITION)[i] = atoi(token);
         token = strtok(NULL, ",");
     }
 
-    free(backup);
-    return 1;
+    safeFree((void **) &backup);
+    return EXIT_SUCCESS;
 }
 
-int parse_activation_func(char *arg, AIFES_E_activations *ACTIVATION_FUNCTION, int TOT_LAYER) {
-    if (ACTIVATION_FUNCTION == NULL)
-        return 0;
+int parse_activation_func(char *arg, AIFES_E_activations **ACTIVATION_FUNCTION, uint32_t TOT_LAYER) {
+    *ACTIVATION_FUNCTION = (AIFES_E_activations *) malloc((TOT_LAYER - 1) * sizeof(AIFES_E_activations));
+    if (*ACTIVATION_FUNCTION == NULL) {
+        safeFree((void **) &ACTIVATION_FUNCTION);
+        printf("Memory allocation failed for ACTIVATION_FUNCTION\n");
+        return EXIT_FAILURE;
+    }
 
     char *token = strtok(arg, ","); // modifies the original string by replacing delimiters with \0 !!!
     for (int i = 0; i < TOT_LAYER - 1; i++) {
         if (token == NULL)
-            return 0;
+            return EXIT_FAILURE;
         if (strcmp(token, "relu") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_relu;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_relu;
         } else if (strcmp(token, "sigmoid") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_sigmoid;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_sigmoid;
         } else if (strcmp(token, "softmax") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_softmax;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_softmax;
         } else if (strcmp(token, "leaky_relu") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_leaky_relu;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_leaky_relu;
         } else if (strcmp(token, "elu") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_elu;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_elu;
         } else if (strcmp(token, "tanh") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_tanh;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_tanh;
         } else if (strcmp(token, "softsign") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_softsign;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_softsign;
         } else if (strcmp(token, "linear") == 0) {
-            ACTIVATION_FUNCTION[i] = AIfES_E_linear;
+            (*ACTIVATION_FUNCTION)[i] = AIfES_E_linear;
         }
         token = strtok(NULL, ",");
     }
-    return 1;
+    return EXIT_SUCCESS;
+}
+
+int initArr(float **arr, uint32_t size, char *initFile) {
+    *arr = (float *) malloc(size * sizeof(float));
+    if (*arr == NULL) {
+        fprintf(stderr, "Error allocating memory for an array (%s)\n", initFile);
+        return EXIT_FAILURE;
+    }
+    if (initFile != NULL && readCSV(initFile, *arr, size) != EXIT_SUCCESS) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+int runTraining(uint32_t *LAYER_COMPOSITION, AIFES_E_activations *ACTIVATION_FUNCTION, uint32_t TOT_LAYER,
+                uint32_t BATCH_SIZE, uint32_t EPOCH, uint32_t LOG_LOSS, float *weights, float *input, float *target,
+                float *output, char *weights_filename) {
+    AIFES_E_model_parameter_fnn_f32 NN_STRUCTURE = {
+        .layer_count = TOT_LAYER,
+        .fnn_structure = LAYER_COMPOSITION,
+        .fnn_activations = ACTIVATION_FUNCTION,
+        .flat_weights = weights,
+    };
+    AIFES_E_init_weights_parameter_fnn_f32 INIT_WEIGHTS = {
+        .init_weights_method = weights_filename != NULL ? AIfES_E_init_no_init : AIfES_E_init_glorot_uniform,
+    };
+
+    AIFES_E_training_parameter_fnn_f32 TRAIN_STRUCTURE = {
+        .optimizer = AIfES_E_adam, // OR AIfES_E_sgd
+        .loss = AIfES_E_mse, // OR AIfES_E_crossentropy
+        .learn_rate = 0.05f,
+        .batch_size = BATCH_SIZE,
+        .epochs = EPOCH,
+        .epochs_loss_print_interval = LOG_LOSS,
+        .loss_print_function = printLoss,
+        .early_stopping = AIfES_E_early_stopping_off, // OR AIfES_E_early_stopping_on
+        .early_stopping_target_loss = 0.004f,
+    };
+
+    uint16_t input_shape[] = {BATCH_SIZE, LAYER_COMPOSITION[0]};
+    uint16_t target_shape[] = {BATCH_SIZE, LAYER_COMPOSITION[TOT_LAYER - 1]};
+
+    aitensor_t input_tensor = AITENSOR_2D_F32(input_shape, input);
+    aitensor_t target_tensor = AITENSOR_2D_F32(target_shape, target);
+    aitensor_t output_tensor = AITENSOR_2D_F32(target_shape, output);
+
+    int8_t error = AIFES_E_training_fnn_f32(&input_tensor, &target_tensor, &NN_STRUCTURE, &TRAIN_STRUCTURE,
+                                            &INIT_WEIGHTS, &output_tensor);
+
+    switch (error) {
+        case 0: fprintf(stdout, "Training OK\n");
+            break;
+        case -1: fprintf(stderr, "ERROR! Tensor dtype\n");
+            break;
+        case -2: fprintf(stderr, "ERROR! Tensor shape: Data Number\n");
+            break;
+        case -3: fprintf(stderr, "ERROR! Input tensor shape does not correspond to ANN inputs\n");
+            break;
+        case -4: fprintf(stderr, "ERROR! Output tensor shape does not correspond to ANN outputs\n");
+            break;
+        case -5: fprintf(stderr, "ERROR! Use the crossentropy as loss for softmax\n");
+            break;
+        case -6: fprintf(stderr, "ERROR! learn_rate or sgd_momentum negative\n");
+            break;
+        case -7: fprintf(stderr, "ERROR! Init uniform weights min - max wrongn");
+            break;
+        case -8: fprintf(stderr, "ERROR! batch_size: min = 1 / max = Number of training data\n");
+            break;
+        case -9: fprintf(stderr, "ERROR! Unknown activation function\n");
+            break;
+        case -10: fprintf(stderr, "ERROR! Unknown loss function\n");
+            break;
+        case -11: fprintf(stderr, "ERROR! Unknown init weights method\n");
+            break;
+        case -12: fprintf(stderr, "ERROR! Unknown optimizer\n");
+            break;
+        case -13: fprintf(stderr, "ERROR! Not enough memory\n");
+            break;
+        default: fprintf(stderr, "Unknown error\n");
+    }
+
+    if (!error) {
+        saveArray(weights_filename, weights, AIFES_E_flat_weights_number_fnn_f32(LAYER_COMPOSITION, TOT_LAYER));
+    }
+
+    return error == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
